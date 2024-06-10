a4823782e0e8ee7b6a13b2bda97f9965fca2c0ef
Marcos Segantine <msegantine8@gmail.com>
2024-06-10 07:59:39 -0300

    feat: initial commit, recording audios

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        string audioUid = GetRandomUid() + ".wav";
+
+        GetAudio(audioUid);
+    }
+
+    public static void GetAudio(string fileName)
+    {
+        using (Process process = new Process())
+        {
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "/usr/bin/arecord",
+                Arguments = "-D plughw:0,0 -f cd -t wav -d 10 /home/marcos/projects/the-rick/audios/" + fileName,
+                RedirectStandardOutput = false,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            process.Start();
+
+            Console.WriteLine("\n\nRecording...\n\n");
+
+            process.WaitForExit();
+        }
+    }
+
+    public static string GetRandomUid()
+    {
+        Guid guid = Guid.NewGuid();
+        return guid.ToString();
+    }
+}
