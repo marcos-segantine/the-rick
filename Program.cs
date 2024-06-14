f513054874596bec5eb96ecebdc0303fac19e158
Marcos Segantine <msegantine8@gmail.com>
2024-06-14 19:32:41 -0300

    feat: adding resorces to proj

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
@@ -9,6 +15,7 @@ class Program
         string audioUid = GetRandomUid() + ".wav";
 
         GetAudio(audioUid);
+        SendFile("./audios/" + audioUid).Wait();
     }
 
     public static void GetAudio(string fileName)
@@ -18,7 +25,7 @@ class Program
             process.StartInfo = new ProcessStartInfo()
             {
                 FileName = "/usr/bin/arecord",
-                Arguments = "-D plughw:0,0 -f cd -t wav -d 10 /home/marcos/projects/the-rick/audios/" + fileName,
+                Arguments = "-D plughw:0,0 -f cd -t wav -d 5 /home/marcos/projects/the-rick/audios/" + fileName,
                 RedirectStandardOutput = false,
                 UseShellExecute = false,
                 CreateNoWindow = true,
@@ -36,5 +43,49 @@ class Program
     {
         Guid guid = Guid.NewGuid();
         return guid.ToString();
+    }
+
+    private static async Task SendFile(string audioPath)
+    {
+        var builder = new ConfigurationBuilder()
+                            .SetBasePath(AppContext.BaseDirectory)
+                            .AddJsonFile("appsettings.json")
+                            .AddEnvironmentVariables("");
+
+        IConfiguration configuration = builder.Build();
+
+
+        var key = configuration["OPEN_AI_KEY"];
+
+        string apiKey = key;
+        string model = "whisper-1";
+
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("https://api.openai.com/v1/audio/transcriptions");
+
+            using (var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                var content = new MultipartFormDataContent();
+
+                var fileContent = new ByteArrayContent(File.ReadAllBytes(audioPath));
+                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/wav");
+                content.Add(fileContent, "file", Path.GetFileName(audioPath));
+
+                content.Add(new StringContent(model), "model");
+
+                request.Content = content;
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
     }
 }
