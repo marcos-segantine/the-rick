e459a393589ec141cd8bde35ecf4efc726d35363
Marcos Segantine <msegantine8@gmail.com>
2024-07-08 07:22:40 -0300

    improving the voice from robot

--- main-pt.py
+++ main-pt.py
@@ -2,10 +2,11 @@ import os
 import json
 import pyaudio
 from vosk import Model, KaldiRecognizer
-
+import boto3
+import pygame
+import io
 import pyttsx3
 from openai import OpenAI
-
 from dotenv import load_dotenv
 
 load_dotenv() 
@@ -17,19 +18,15 @@ engine.setProperty("rate", 120)
 client = OpenAI(api_key=os.getenv("OPEAN_AI_KEY"))
 
 def recognize_speech_from_microphone():
-    # Path to the downloaded Vosk model
     model_path = "vosk-model-small-pt-0.3"
     
-    # Check if the model path exists
     if not os.path.exists(model_path):
         print("Model path does not exist.")
         return
 
-    # Load the Vosk model
     model = Model(model_path)
     recognizer = KaldiRecognizer(model, 16000)
 
-    # Initialize PyAudio
     audio = pyaudio.PyAudio()
     stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
     stream.start_stream()
@@ -59,14 +56,41 @@ def recognize_speech_from_microphone():
             )
             
             print(completion.choices[0].message.content)
+            return completion.choices[0].message.content
             
-            text_to_speech(completion.choices[0].message.content)
-
 def text_to_speech(text):
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-    engine.say(text)
-    engine.runAndWait()
+    polly = boto3.client('polly', 
+                     region_name='sa-east-1', 
+                     aws_access_key_id=aws_access_key_id, 
+                     aws_secret_access_key=aws_secret_access_key)
+
+    response = polly.synthesize_speech(
+        Text=text,
+        OutputFormat='mp3',
+        VoiceId='Camila'
+    )
+    
+    audio_stream = response['AudioStream'].read()
+    return audio_stream
+
+def play_audio(audio_stream):
+    pygame.mixer.init()
+    
+    audio_file = io.BytesIO(audio_stream)
+    
+    pygame.mixer.music.load(audio_file, 'mp3')
+    
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
 
 
 if __name__ == "__main__":
-    recognize_speech_from_microphone()
+  while True:
+      text = recognize_speech_from_microphone()
+      audio_stream = text_to_speech(text)
+      play_audio(audio_stream)
